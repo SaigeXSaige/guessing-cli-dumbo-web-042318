2d210bb233e08b2356102eace61dfe7545d6d285
Saige Leslie <saigeleslie@gmail.com>
2018-04-16 20:54:01 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,6 +4,8 @@ def run_guessing_game
   user_response = gets.chomp 
   
   if user_response == i
-    puts 
+    puts "You guessed the correct number!"
+  else
+    puts "The computer guessed #{i}"
   
 end
